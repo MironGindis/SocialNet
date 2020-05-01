926d177338b93ad59f129121ab0d1ef2e6a35b1f
Miron Gindis <mge1999@yaqndex.ru>
2020-05-01 17:53:50 +0500

    Updates

---
+++ precache-manifest.8c6426fc6f74578aa99aefca7b0e22bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ec622b8bf1a750210d28222545e92b",
+    "url": "/MironGindis/SocialNet/index.html"
+  },
+  {
+    "revision": "15ec836f6e146e328d1c",
+    "url": "/MironGindis/SocialNet/static/css/main.5e533d3a.chunk.css"
+  },
+  {
+    "revision": "53a3e5b9e9c11af697df",
+    "url": "/MironGindis/SocialNet/static/js/2.66718bc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MironGindis/SocialNet/static/js/2.66718bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ec836f6e146e328d1c",
+    "url": "/MironGindis/SocialNet/static/js/main.ef47ac0c.chunk.js"
+  },
+  {
+    "revision": "a5cfb74e5fcad4b79b9f",
+    "url": "/MironGindis/SocialNet/static/js/runtime-main.867bdc66.js"
+  },
+  {
+    "revision": "3aedb725c4ba7949b5f451b325dad273",
+    "url": "/MironGindis/SocialNet/static/media/91.3aedb725.svg"
+  },
+  {
+    "revision": "cefea5a45da8065b52db363b41ffc963",
+    "url": "/MironGindis/SocialNet/static/media/User-Icon.cefea5a4.png"
+  },
+  {
+    "revision": "3a05d0eed13eb7be4c7d898746c828ff",
+    "url": "/MironGindis/SocialNet/static/media/chill.3a05d0ee.webp"
+  },
+  {
+    "revision": "98c224970864a43bf5bce0f286fb2823",
+    "url": "/MironGindis/SocialNet/static/media/trruejob.98c22497.webp"
+  }
+]);
